3b641859c2642e0c1976a0b98d289a7ad6241b75
huntersurf <huntersurf92@gmail.com>
2021-02-15 21:24:50 -0800

    Commit at 9:24pm 2/15/2021 Hunter using VMWare

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,6 +13,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		FizzBuzz fb = new FizzBuzz();
-		System.out.println(fb.toString());
+		//System.out.println(fb.toString());
 	}
 }
